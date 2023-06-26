c05e36b4afd83825ff80f035b3c2d00294a48eb7
Nastya <asenka0301@gmail.com>
2023-06-26 11:52:50 +0300

    add getRate

--- src/API/CurrencyService.js
+++ src/API/CurrencyService.js
@@ -6,7 +6,16 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export default class CurrencyService {
   static async getRates(baseCurrency) {
     try {
-      const response = await axios.get(`https://api.apilayer.com/fixer/latest&base=${baseCurrency}&symbols`, { headers: { apikey: API_KEY } });
+      const response = await axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols&base=${baseCurrency}`, { headers: { apikey: API_KEY } });
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  static async getRate(base, symbols) {
+    try {
+      const response = await axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols=${symbols}&base=${base}`, { headers: { apikey: API_KEY } });
       return response;
     } catch (e) {
       console.log(e);
