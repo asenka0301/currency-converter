4b6754e2af72495d2bbeebd479d30139d73c7f3b
Nastya <asenka0301@gmail.com>
2023-07-07 12:14:57 +0300

    add: hide api key using node.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json('hi');
+});
+
+app.get('/rates', async (req, res) => {
+  const base = req.query.baseCurrency;
+  try {
+    const response = await axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols&base=${base}`, { headers: { apikey: process.env.REACT_APP_API_KEY } });
+    res.json(response.data);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get('/convert', async (req, res) => {
+  const baseCurrency = req.query.base;
+  const currencies = req.query.symbols;
+  try {
+    console.log(req.query);
+    const response = await axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols=${currencies}&base=${baseCurrency}`, { headers: { apikey: process.env.REACT_APP_API_KEY } });
+    res.json(response.data);
+    console.log(response.data);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`));
--- src/API/CurrencyService.js
+++ src/API/CurrencyService.js
@@ -1,12 +1,10 @@
 /* eslint-disable consistent-return */
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-
 export default class CurrencyService {
   static async getRates(baseCurrency) {
     try {
-      const response = await axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols&base=${baseCurrency}`, { headers: { apikey: API_KEY } });
+      const response = await axios.get('http://localhost:8000/rates', { params: { baseCurrency } });
       return response;
     } catch (e) {
       console.log(e);
@@ -15,7 +13,7 @@ export default class CurrencyService {
 
   static async getRate(base, symbols) {
     try {
-      const response = await axios.get(`https://api.apilayer.com/exchangerates_data/latest?symbols=${symbols}&base=${base}`, { headers: { apikey: API_KEY } });
+      const response = await axios.get('http://localhost:8000/convert', { params: { base, symbols } });
       return response;
     } catch (e) {
       console.log(e);
