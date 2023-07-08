3429b5f9de3bcfbbd80bf8d74e8d5dd6597e513c
Nastya <asenka0301@gmail.com>
2023-07-08 16:51:32 +0300

    change server url

--- src/API/CurrencyService.js
+++ src/API/CurrencyService.js
@@ -1,10 +1,13 @@
 /* eslint-disable consistent-return */
 import axios from 'axios';
 
+// const url = 'http://localhost:4000'; // dev
+const url = 'https://currency-converter-backend.onrender.com';
+
 export default class CurrencyService {
   static async getRates(baseCurrency) {
     try {
-      const response = await axios.get('http://localhost:4000/rates', { params: { baseCurrency } });
+      const response = await axios.get(`${url}/rates`, { params: { baseCurrency } });
       return response;
     } catch (e) {
       console.log(e);
@@ -13,7 +16,7 @@ export default class CurrencyService {
 
   static async getRate(base, symbols) {
     try {
-      const response = await axios.get('http://localhost:4000/convert', { params: { base, symbols } });
+      const response = await axios.get(`${url}/convert`, { params: { base, symbols } });
       return response;
     } catch (e) {
       console.log(e);
